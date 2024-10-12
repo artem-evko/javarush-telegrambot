130df8db3a5ed48dd0d39a73fa2eb0ae459db542
artem <evko_artem@mail.ru>
2024-10-12 14:41:07 +0300

    JRTB-7: added the ability to delete group subscription.

--- src/main/java/com/github/javarushcommunity/jrtb/command/HelpCommand.java
+++ src/main/java/com/github/javarushcommunity/jrtb/command/HelpCommand.java
@@ -13,7 +13,7 @@ public class HelpCommand extends AbstractCommand{
     private final static String commandIdentifier = "/help";
 
 
-    public static final String HELP_MESSAGE= String.format("✨Дотупные команды✨\n\n"
+    public static final String HELP_MESSAGE = String.format("✨Дотупные команды✨\n\n"
 
                     + "Начать\\закончить работу с ботом:\n"
                     + "%s - начать работу со мной\n"
@@ -21,12 +21,14 @@ public class HelpCommand extends AbstractCommand{
 
                     + "Работа с подписками на группы:\n"
                     + "%s - подписаться на группу статей\n"
+                    + "%s - отписаться от группы статей\n"
                     + "%s - получить список групп, на которые подписан\n\n"
 
                     + "%s - получить помощь в работе со мной\n"
                     + "%s - получить мою статистику использования\n",
             START.getCommandName(), STOP.getCommandName(), ADD_GROUP_SUB.getCommandName(),
-            LIST_GROUP_SUB.getCommandName(), HELP.getCommandName(), STAT.getCommandName());
+            DELETE_GROUP_SUB.getCommandName(), LIST_GROUP_SUB.getCommandName(), HELP.getCommandName(),
+            STAT.getCommandName());
 
     @Override
     public SendMessage buildResponse(Update update) {
